25db8c1fca72ae806c5380422d845bf528b0e99f
cnunn1 <cnunn1@ivc.edu>
2017-02-22 22:36:15 -0800

    Create date.h

---
+++ date.h
@@ -0,0 +1,36 @@
+#include "header.h"
+
+class date
+{
+  public:
+    // Constructors
+    date();
+    date(int, int, int);
+
+    // Gets
+    int getDay() const{
+    return day;
+    }
+    int getMonth() const{
+    return month;
+    }
+    int getYear() const{
+    return year;
+    }
+
+    // Sets
+    void setDay(int d){
+    day = d;
+    }
+    void setMonth(int m){
+    month = m;
+    }
+    void setYear(int y){
+    year = y;
+    }
+
+  private:
+    int day;
+    int month;
+    int year;
+} // End class date
