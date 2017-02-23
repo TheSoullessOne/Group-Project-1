47a06e723bf984b65a3f1bbb2b3a099a137b93bc
cnunn1 <cnunn1@ivc.edu>
2017-02-22 22:37:11 -0800

    Create member.h

---
+++ member.h
@@ -0,0 +1,78 @@
+#include "header.h"
+
+class member
+{
+  public:
+    // Constructors
+    member();
+    member(string, int, bool, date, double, double, item*);
+
+    // Gets
+    string getName() const{
+    return name;
+    }
+    int getNum() const{
+    return num;
+    }
+    bool getType() const{
+    return type;
+    }
+    date getExpiry() const{
+    return expiry;
+    }
+    double getTotal() const{
+    return total;
+    }
+    double getAnnual() const{
+    return annual_dues;
+    }
+    item* getReceipt() const{
+    return receipt;
+    }
+
+    // Sets
+    void setName(string na){
+    name = na;
+    }
+    void setNum(int nu){
+    num = nu;
+    }
+    void setType(bool ty){
+    type = ty;
+    }
+    void setExpiry(int d, int m, int y){
+    date::setDay(d);
+    date::setMonth(m);
+    date::setYear(y);
+    }
+    void setTotal(double to){
+    total = to;
+    }
+    void setAnnual(double a){
+    annual_dues = a;
+    }
+    void setReceipt(item* r){
+    receipt = r;
+    }
+
+    // Operator Overloads
+    /*** Satisfy the Overload Requirement ***/
+
+
+
+
+
+  private:
+    string name;
+    int    num;          // Membership #
+    bool   type;         // Membership Type:
+                         // False = Regular Member
+                         // True  = Executive Member
+    date   expiry;       // Membership Expiration Date
+                         /*** Satisfies Composition Requirement ***/
+    double total;        // Total Amount Spent
+    double annual_dues;  // Regular   Member: $85.00
+                         // Executive Member: $95.00
+    item*  receipt;      // Will attempt to keep track of members' purchases
+                         /*** Satisfies Pointer Requirement ***/
+} // End class member
