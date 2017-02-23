062ec9e4b63efa597e447f0149467674f5767bb5
cnunn1 <cnunn1@ivc.edu>
2017-02-22 22:36:43 -0800

    Create item.h

---
+++ item.h
@@ -0,0 +1,14 @@
+#include "header.h"
+
+class item
+{
+  public:
+    // Constructors
+    item();
+    item(string*, double*, date*);
+
+  private:
+    string* names;
+    double* prices;       // names & prices are intended to be parallel arrays
+    date    shopping_day;
+}
