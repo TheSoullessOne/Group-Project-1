c4f9a7fcbc1e7c95e014f191703131b03b424bff
Brian <btdeaquino@gmail.com>
2017-03-01 00:11:08 -0800

    Created the search function utilizing vectors. You can combine linked lists and vectors if you #include <list> and #include <vector>.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <iomanip>
+#include <QDebug>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +17,33 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    // copy_if example
+
+    bool is_positive(int i);
+
+    vector<int> memberSearch = {};//add infor for members.
+    vector<int> expiringMember (memberSearch.size());
+
+    // copy only expiring members
+    auto it = copy_if (memberSearch.begin(), memberSearch.end(), expiringMember.begin(),  is_positive);
+    expiringMember.resize(distance(expiringMember.begin(),it));  // shrink container to new members
+
+    cout << "Expiring members contains:";
+    for (int& x: expiringMember)
+    {
+     cout << ' ' << x;
+     cout << '\n';
+    }
+
+
+}
+
+bool is_positive(int i)
+{
+    return !(i<0);
 }
