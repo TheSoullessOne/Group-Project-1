5ca74fcf3a5d431b33d710ef834bbff278b4d0d2
khaller1 <kaila.haller@yahoo.com>
2017-02-28 17:36:26 -0800

    Rebate.cpp

---
+++ membershipstatusrebate.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+#include "header.h"
+#include <QChar>
+#include <QDebug>
+
+void membershipStatus( ){
+
+
+}
