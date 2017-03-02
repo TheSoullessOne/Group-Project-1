f7d2a8b4b46c8f66d2889fbad8e0c7aefe169c81
surydee <desserystarship@gmail.com>
2017-03-01 19:49:15 -0800

    create a second window i think not sure if it works yet lol

---
+++ addrecwindow.h
@@ -0,0 +1,25 @@
+#ifndef ADDRECWINDOW_H
+#define ADDRECWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class addRecWindow;
+}
+
+class addRecWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit addRecWindow(QWidget *parent = 0);
+    ~addRecWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui:addRecWindow *ui;
+};
+
+#endif // ADDRECWINDOW_H
