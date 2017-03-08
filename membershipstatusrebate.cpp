190b99e7c0e77e5e451b3653f3b9f7af654189a5
khaller1 <kaila.haller@yahoo.com>
2017-03-07 16:57:21 -0800

    skeleton for membership rebate func

---
+++ membershipstatusrebate.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+#include "header.h"
+#include <QChar>
+#include <QDebug>
+
+//pass in id - string
+void memberRebate(QString memberId){
+
+    //utilize a search function
+
+    bool executive_yes = false;
+
+    //based on user the bool will change
+
+    if(executive_yes == true){
+        //there will be a rebate
+        //annual membership price is standard PLUS rebate
+    }
+    else{
+        //the rebate is 0 for a non exec member
+        //annual membership price is standard
+    }
+
+}
