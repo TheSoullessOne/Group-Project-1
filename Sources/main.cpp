07370cd48807b66f8603b6a4e7965871f5c3dbff
surydee <desserystarship@gmail.com>
2017-03-07 17:16:12 -0800

    uncmmmenting int main

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,17 +1,17 @@
 #include "Headers\mainwindow.h"
 #include <QApplication>
 
-//int main(int argc, char *argv[])
-//{
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-//    // These are just test functions trying to figure
-//    // out how vectors and files work. -Eric
-////    InitializeVectors();
-////    UpdateDataFromFile("day1.txt");
+    // These are just test functions trying to figure
+    // out how vectors and files work. -Eric
+//    InitializeVectors();
+//    UpdateDataFromFile("day1.txt");
 
 
-//    return a.exec();
-//}
+    return a.exec();
+}
