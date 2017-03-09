afa7cd45ab35c15c1238679ab6b11b10997de235
khaller1 <kaila.haller@yahoo.com>
2017-03-08 23:41:52 -0800

    commenting to plan out rebate calculation

--- Sources/membershipstatusrebate.cpp
+++ Sources/membershipstatusrebate.cpp
@@ -10,6 +10,8 @@
 void membershipStatus(QString id){
 
     bool executive_yes = false;
+    //rebate is based off of total purchases for year
+    //so we need a running total variable for purchases
 
     //use a search function using the member's id
     //to determine what kind of member they are
@@ -18,7 +20,14 @@ void membershipStatus(QString id){
 
     if(executive_yes = true){
         //annual executive membership is $95
-        //this will be added to their rebate
+        //the rebate will be subtracted from this charge
+        //rebate = 3.25% * subtotal (before tax)
+        //if(month is Dec){
+        //  rebate will be paid out
+        //}
+        //use a for loop to add the totals from each month
+        //to create a yearly total?
+
     }
     else if(executive_yes = false){
         //annual regular membership is $85
